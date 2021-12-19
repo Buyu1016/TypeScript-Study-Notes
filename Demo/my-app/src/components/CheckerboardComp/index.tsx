998883b4ab1b1e25ac7e5de55c870cca7d6c5eb1
CodeGorgeous <2460481461@qq.com>
2021-12-19 22:35:01 +0800

    提交

--- Demo/my-app/src/components/CheckerboardComp/index.tsx
+++ Demo/my-app/src/components/CheckerboardComp/index.tsx
@@ -1,32 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PieceComp from '../PieceComp'
 import computedSize from '../../utils/computedSize'
+import { GameState, PieceState } from '../../types/enums'
+import getGameState from '../../utils/getGameState'
 
 interface Props {
     size: number
-    state: "red" | "black"
+    state: PieceState.red | PieceState.black
     onChange: () => void
+    ifAgain?: boolean
+    gameState: GameState
+    onChangeGameState: (gameState: GameState) => void
 }
 
 const Component: React.FC<Props> = (props) => {
-    const [lock, setLock] = useState(false);
-    let [boardData, setBoardData] = useState(computedSize(props.size, computedSize(props.size, "empty")));
-    const vNode = boardData.flat().map((item: 'empty' | 'red' | 'black', index: number) => {
+
+    let [boardData, setBoardData] = useState(computedSize(props.size, computedSize(props.size,PieceState.empty)));
+    const vNode = boardData.flat().map((item: PieceState, index: number) => {
         return (
             <PieceComp
                 state={item}
                 key={index}
                 onClick={() => {
-                    let x = index % props.size;
-                    let y = ( index - x ) / props.size;
-                    if (boardData[y][x] !== 'empty') return
+                    const x: number = index % props.size;
+                    const y: number = ( index - x ) / props.size;
+                    if (props.gameState === GameState.redWin || props.gameState === GameState.blackWin || props.gameState === GameState.draw) return;
+                    if (boardData[y][x] !== PieceState.empty) return
                     boardData[y][x] = props.state;
-                    setLock(!lock);
+                    const result = getGameState(boardData.flat(), index, props.gameState, props.size);
+                    props.onChangeGameState(result);
                     props.onChange();
                 }}
             />
         )
     });
+
+    useEffect(() => {
+        setBoardData(computedSize(props.size, computedSize(props.size, PieceState.empty)));
+        props.onChangeGameState(GameState.playing);
+    }, [props.ifAgain])
     
     return (
         <div
